768aae25812df6d9e031ce95fcb23e096304a32a
Elvis Duru <duruelvis95@gmail.com>
2019-05-11 19:14:13 +0100

    ...

---
+++ src/Connect/Chats/PrivateChat/PrivateChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivateChat = () => {
+  return (
+    <div className={styles.PrivateChat}>
+      <div className={styles.Header} />
+    </div>
+  );
+};
+
+export default PrivateChat;
