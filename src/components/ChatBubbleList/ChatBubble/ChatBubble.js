df857c086737246453917191e9d77332737ac008
Elvis Duru <duruelvis95@gmail.com>
2019-05-15 19:21:20 +0100

    chat bubble

--- src/components/ChatBubbleList/ChatBubble/ChatBubble.js
+++ src/components/ChatBubbleList/ChatBubble/ChatBubble.js
@@ -4,25 +4,19 @@ import styles from "./ChatBubble.module.css";
 const ChatBubble = props => {
   let message;
 
+  let floatLeftStyle = props.message.sender === "you" ? styles["left"] : null;
+
   if (props.message.type === "text") {
-    message = (
-      <p className={props.message.sender === "you" ? styles.left : null}>
-        {props.message.content}
-      </p>
-    );
+    message = <p>{props.message.content}</p>;
   }
 
   if (props.message.type === "image") {
-    message = (
-      <img
-        className={props.message.sender === "you" ? styles.left : null}
-        src={props.message.content}
-        alt=""
-      />
-    );
+    message = <img src={props.message.content} alt="" />;
   }
 
-  return <div className={styles.ChatBubble}>{message}</div>;
+  return (
+    <div className={floatLeftStyle + " " + styles.ChatBubble}>{message}</div>
+  );
 };
 
 export default ChatBubble;
